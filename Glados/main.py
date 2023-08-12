8859010ecf51d93f043b6018f8891002f88f0e40
TheTrollGodJr <thetrollgodjr@gmail.com>
2023-08-12 15:12:18 -0600

    glados, discord bot, .gitignore

---
+++ Glados/main.py
@@ -0,0 +1,11 @@
+import torch
+import torchaudio
+import torch.nn as nn
+import torch.nn.functional as F
+
+import IPython
+
+from tortoise.api import TextToSpeech
+from tortoise.utils.audio import load_audio, load_voice, load_voices
+
+tts = TextToSpeech()
